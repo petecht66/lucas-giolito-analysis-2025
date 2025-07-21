86786faeb50cf40032f43537958b3e53200af988
Peter <petecht66@gmail.com>
2025-07-21 12:52:02 -0400

    starting mechanics script

--- scripts/pitch_mechanics.r
+++ scripts/pitch_mechanics.r
@@ -1,3 +1,3 @@
 
 # import data from .csv file
-mechanics_data <- read.csv("https://raw.githubusercontent.com/petecht66/lucas-giolito-analysis/main/data/Lucas_Giolito_Arm_Angle_and_Mound_Data.csv")
+mechanics_data <- read.csv("https://raw.githubusercontent.com/petecht66/lucas-giolito-analysis-2025/refs/heads/main/data/Lucas%20Giolito%20Arm%20Angle%20and%20Mound%20Data.csv")
