c40d2ea76dae276edc0f2429bd8687e9267a589d
Peter <petecht66@gmail.com>
2025-07-26 02:29:37 -0400

    creation of second script

---
+++ scripts/pitch_mix.r
@@ -0,0 +1,4 @@
+
+# import data from .csv file
+performance_data <- read.csv("https://raw.githubusercontent.com/petecht66/lucas-giolito-analysis-2025/refs/heads/main/data/Lucas%20Giolito%20Pitch%20Mix%20and%20Outcomes.csv")
+
