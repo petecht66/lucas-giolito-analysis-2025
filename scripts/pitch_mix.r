328c7ffdcf1e571c92a3733a3e8133bb6a3962df
Peter <petecht66@gmail.com>
2025-07-27 07:08:17 -0400

    first plot: strike percentage

--- scripts/pitch_mix.r
+++ scripts/pitch_mix.r
@@ -7,8 +7,15 @@ performance_data <- read.csv("https://raw.githubusercontent.com/petecht66/lucas-
 # creating a column for starts with old arm angle versus new arm angle, similar to other file
 performance_data$Angle <- ifelse(performance_data$StartNum <= 7, "Old Arm Angle", "New Arm Angle")
 
-# plotting arm angle by start number while identifying where arm angle change was
-ggplot(performance_data, aes(x = StartNum, y = Strike%, color = Angle)) +
-geom_point() + geom_line() + ggtitle("Strike Percentage by Start") +
-labs(x = "Start Number", y = "Strike%") +
-theme_minimal() + theme(plot.title = element_text(hjust = 0.5))
+# plotting strike percentage by start number while identifying where arm angle change was
+ggplot(performance_data, aes(x = StartNum, y = StrikePct, color = Angle)) +
+  geom_point() + 
+  geom_line() + 
+  geom_hline(yintercept = 64.5, linetype = "dashed", color = "black") +
+  annotate("text", x = max(performance_data$StartNum), y = 64.5, 
+           label = "League Avg", vjust = -0.5, hjust = 1, size = 3) +
+  ggtitle("Strike Percentage by Start") +
+  labs(x = "Start Number", y = "Strike%") +
+  theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0.5))
+
